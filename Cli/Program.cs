29da73680a03a5ff840b5493d9b24966ed9e617d
Evgeni Nabokov <nabokov-ef@yandex.ru>
2018-02-14 08:19:02 -0800

    Started implementing of parsing of a YAML file with the parameters for generation.

--- Cli/Program.cs
+++ Cli/Program.cs
@@ -3,15 +3,25 @@ using System.IO;
 using Lib;
 using Lib.CharMappers;
 using Lib.PasswordPattern;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+using YamlDotNet.Serialization.Utilities;
 
 namespace Cli
 {
     internal class Program
     {
-        private static string filename = "combinations.key";
+        private static string filename = "combinations.txt";
 
         static void Main(string[] args)
         {
+            var deserializer = new DeserializerBuilder()
+                                    .WithNamingConvention(new CamelCaseNamingConvention())
+                                    //.WithTypeConverter(new TypeConverter())
+                                    .IgnoreUnmatchedProperties()
+                                    .Build();
+            var patternParams = deserializer.Deserialize<PatternParams>(File.ReadAllText(args[0]));
+
             var passwordPattern = new PasswordPatternBuilder(2)
                 .AddArbitraryPasswordSection("01", 3, 2, CharCase.UpperAndLower, new RussianToEnglishMapper())
                 .Build();
@@ -37,5 +47,33 @@ namespace Cli
         }
     }
 
-    
+    public class PatternParams
+    {
+        public string OutputFilename { get; set; }
+        public int? MaxSingeCharSequence { get; set; }
+        public SectionParamsBase[] Sections { get; set; }
+    }
+
+
+    public abstract class SectionParamsBase
+    {
+        public string Chars { get; set; }
+        public string CharMapper { get; set; }
+        public CharCase CharCase { get; set; }
+    }
+
+    public class ArbitrarySectionParams : SectionParamsBase
+    {
+        public int MaxLength { get; set; }
+        public int MinLength { get; set; }
+    }
+
+    public class FixedSectionParams : SectionParamsBase
+    {
+    }
+
+    //public class TypeConverter : IYamlTypeConverter
+    //{
+        
+    //}
 }
